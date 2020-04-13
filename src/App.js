4877df54dc45c84b432cff097fd39f4ae2f34fb1
Roy Nguyen <roynguyen147@gmail.com>
2020-04-13 08:55:34 +0700

    xong VD useEffect => vẫn sử dụng shallow Equal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [data, setData] = useState({ id: 1, name: 'Kiet' })
+  // RESULT: data thay đổi vùng nhớ => useEffect vẫn triger lại => shallow Equal
+  useEffect(() => {
+    console.log('%c%s', 'color: #ec0202; background: yellow; font-size: 10px', JSON.stringify('Triggered action', null, 2))
+  }, [data])
+
+  const setTheSameValue = () => {
+    console.log(
+      '%c%s',
+      'color: ##0e0091; background: yellow; font-size: 10px',
+      JSON.stringify('setTheSameValue', null, 2)
+    )
+    setData({ id: 1, name: 'Kiet' })
+  }
+
+  // RESULT: works => useEffect watching 1 biến thì vẫn xài shallowEqual
+  const [binaryData, setBinaryData] = useState('Kiet')
+  useEffect(() => {
+    console.log(
+      '%c%s',
+      'color: #ec0202; background: yellow; font-size: 10px',
+      JSON.stringify('Triggered action', null, 2)
+    )
+  }, [binaryData])
+
+  const setTheSameValueBinary = () => {
+    console.log(
+      '%c%s',
+      'color: ##0e0091; background: yellow; font-size: 10px',
+      JSON.stringify('setTheSameValueBinary', null, 2)
+    )
+    setBinaryData('Kiet')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +44,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={setTheSameValue}>
+          Click to edit data to the same object
+        </button>
+        <button onClick={setTheSameValueBinary}>
+          Click to edit data to the same binary data
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +60,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
